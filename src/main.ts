6c8600f38b955d3ffc6febce4851d7555c0ac85d
fakhriazdev <fakhriazdev@gmail.com>
2025-08-01 22:04:03 +0700

    [FIX]:CORS nest at vercel

--- src/main.ts
+++ src/main.ts
@@ -16,29 +16,7 @@ async function bootstrap(): Promise<void> {
   app.enableCors({
     origin: 'https://task-manager-fe-lyart.vercel.app',
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
-    allowedHeaders: [
-      'Origin',
-      'X-Requested-With',
-      'Content-Type',
-      'Accept',
-      'Authorization',
-      'Authentication',
-      'Access-Control-Allow-Credentials',
-      'Access-Control-Allow-Origin',
-      'Access-Control-Allow-Methods',
-      'Access-Control-Allow-Headers',
-      'Access-Control-Request-Headers',
-      'Access-Control-Request-Method',
-      'X-CSRF-Token',
-      'X-Access-Token',
-      'X-Auth-Token',
-      'X-Client-Id',
-      'X-Frame-Options',
-      'X-Forwarded-For',
-      'X-Real-IP',
-      'X-Requested-With',
-    ],
-
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
   });
 
